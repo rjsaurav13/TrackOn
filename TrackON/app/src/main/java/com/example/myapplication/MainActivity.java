792ce19dfac51087b9e1326c879b7c11cd3cd71f
rjsaurav13 <rjsaurav13@gmail.com>
2022-08-25 15:59:24 +0530

    Update MainActivity.java

--- TrackON/app/src/main/java/com/example/myapplication/MainActivity.java
+++ TrackON/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN=3000;
+    //variables
+    Animation topAnim;
+    Animation bottomAnim;
+    ImageView image;
+    TextView growchat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        image=findViewById(R.id.image);
+        growchat=findViewById(R.id.Head);
+
+
+        image.setAnimation(topAnim);
+        image.setAnimation(bottomAnim);
+
+        growchat.setAnimation(topAnim);
+        growchat.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
     }
 }
