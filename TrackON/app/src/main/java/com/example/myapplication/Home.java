171996dbf214b31e7f51b8a295cfae110ad737ff
rjsaurav13 <rjsaurav13@gmail.com>
2022-08-27 19:47:03 +0530

    Update Home.java

--- TrackON/app/src/main/java/com/example/myapplication/Home.java
+++ TrackON/app/src/main/java/com/example/myapplication/Home.java
@@ -9,6 +9,7 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
     }
 }
