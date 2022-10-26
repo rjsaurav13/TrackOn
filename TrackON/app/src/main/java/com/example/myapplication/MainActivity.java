c5fa2bfe529aee7e973b59f44baa23c8db8f70c0
rjsaurav13 <rjsaurav13@gmail.com>
2022-10-26 15:29:44 +0530

    Update MainActivity.java

--- TrackON/app/src/main/java/com/example/myapplication/MainActivity.java
+++ TrackON/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide(); // hide the title bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
