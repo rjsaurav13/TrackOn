36195ba2aaec83202235c010bee63c01f69f823c
rjsaurav13 <rjsaurav13@gmail.com>
2022-10-28 01:11:37 +0530

    Update Home.java

--- TrackON/app/src/main/java/com/example/myapplication/Home.java
+++ TrackON/app/src/main/java/com/example/myapplication/Home.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
 
@@ -11,5 +14,16 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
+        ImageView imageView;
+
+            imageView = findViewById(R.id.log);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent intent = new Intent(Home.this, Login.class);
+                    startActivity(intent);
+                }
+            });
+        }
     }
-}
