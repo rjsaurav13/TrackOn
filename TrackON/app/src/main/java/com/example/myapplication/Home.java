588491ccae180207a885577bf0fc5d522cb62083
rjsaurav13 <rjsaurav13@gmail.com>
2022-10-29 12:33:15 +0530

    Update Home.java

--- TrackON/app/src/main/java/com/example/myapplication/Home.java
+++ TrackON/app/src/main/java/com/example/myapplication/Home.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
+
 
 public class Home extends AppCompatActivity {
 
@@ -14,27 +16,24 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
-        ImageView imageView;
+        Button login= findViewById(R.id.signin);
+        Button signup = (Button) findViewById(R.id.reg);
+        signup.setMovementMethod(LinkMovementMethod.getInstance());
 
-            imageView = findViewById(R.id.log);
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    Intent intent = new Intent(Home.this, Login.class);
-                    startActivity(intent);
-                }
-            });
-            ImageView imageView1;
-
-            imageView1 = findViewById(R.id.reg);
-            imageView1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    Intent intent = new Intent(Home.this, Register.class);
-                    startActivity(intent);
-                }
-            });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Home.this,Login.class);
+                startActivity(i);
             }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent viewIntent=new Intent(Home.this,Register.class);
+                startActivity(viewIntent);
+            }
+        });
+
     }
+}
