9fd03e81b28c57f0dc5851f8c0519bd729229f39
rjsaurav13 <rjsaurav13@gmail.com>
2022-10-30 03:30:07 +0530

    Update Login.java

--- TrackON/app/src/main/java/com/example/myapplication/Login.java
+++ TrackON/app/src/main/java/com/example/myapplication/Login.java
@@ -3,13 +3,90 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.util.Patterns;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class Login extends AppCompatActivity {
 
+    private EditText user_name, pass_word;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+//        getSupportActionBar().hide(); // hide the title bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        TextView t1 = (TextView) findViewById(R.id.textView);
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login.this,Register.class);
+                startActivity(i);
+            }
+        });
+
+        user_name=findViewById(R.id.email);
+        pass_word=findViewById(R.id.pass);
+        Button btn_login = findViewById(R.id.button2);
+        mAuth=FirebaseAuth.getInstance();
+        btn_login.setOnClickListener(v -> {
+            String email= user_name.getText().toString().trim();
+            String password=pass_word.getText().toString().trim();
+            if(email.isEmpty())
+            {
+                user_name.setError("Email is empty");
+                user_name.requestFocus();
+                return;
+            }
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+            {
+                user_name.setError("Enter the valid email");
+                user_name.requestFocus();
+                return;
+            }
+            if(password.isEmpty())
+            {
+                pass_word.setError("Password is empty");
+                pass_word.requestFocus();
+                return;
+            }
+            if(password.length()<6)
+            {
+                pass_word.setError("Length of password is more than 6");
+                pass_word.requestFocus();
+                return;
+            }
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                if(task.isSuccessful())
+                {
+                    startActivity(new Intent(Login.this, track.class));
+                }
+                else
+                {
+                    Toast.makeText(Login.this,
+                            "Please Check Your login Credentials",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            });
+        });
     }
+
 }
