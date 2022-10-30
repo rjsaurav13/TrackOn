4894ac1c74ba62d269590edd3efae52c9c90713d
rjsaurav13 <rjsaurav13@gmail.com>
2022-10-30 14:31:13 +0530

    Update track.java

--- TrackON/app/src/main/java/com/example/myapplication/track.java
+++ TrackON/app/src/main/java/com/example/myapplication/track.java
@@ -9,6 +9,7 @@ public class track extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_track);
     }
 }
