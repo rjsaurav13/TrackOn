763ef166eaa7cfa6bbad43a4702d2d1ce80ee603
Priyodit Choudhary <priyoditseervi1432@gmail.com>
2022-11-09 23:26:19 +0530

    Update sketch_nov6b.ino

--- sketch_nov6b.ino
+++ sketch_nov6b.ino
@@ -1,13 +1,4 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/lilygo-t-sim7000g-esp32-lte-gprs-gps/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
+
 
 // Based on the following example: https://github.com/Xinyuan-LilyGO/LilyGO-T-SIM7000G/blob/master/examples/Arduino_TinyGSM/AllFunctions/AllFunctions.ino
 
