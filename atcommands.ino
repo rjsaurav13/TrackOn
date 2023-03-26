e5840f4679dd2c4faef3848011f14ce1b0e294cb
Saurav Kumar <67797045+rjsaurav13@users.noreply.github.com>
2023-03-26 21:49:38 +0530

    Update atcommands.ino

--- atcommands.ino
+++ atcommands.ino
@@ -1,240 +1,80 @@
-#define TINY_GSM_MODEM_SIM7000
+/*
+  FILE: ATdebug.ino
+  AUTHOR: Koby Hale
+  PURPOSE: test AT commands
+  List of SIM7000 AT commands can be found here
+  http://www.microchip.ua/simcom/LTE/SIM7000/SIM7000%20Series_AT%20Command%20Manual_V1.05.pdf
+*/
 
-// Set serial for debug console (to the Serial Monitor, default speed 115200)
-#define SerialMon Serial
-
-// Set serial for AT commands (to the module)
-// Use Hardware Serial on Mega, Leonardo, Micro
 #define SerialAT Serial1
 
-// See all AT commands, if wanted
-//#define DUMP_AT_COMMANDS
-
-// Define the serial console for debug prints, if needed
-#define TINY_GSM_DEBUG SerialMon
-
-// Add a reception delay, if needed.
-// This may be needed for a fast processor at a slow baud rate.
-// #define TINY_GSM_YIELD() { delay(2); }
-
-// Define how you're planning to connect to the internet
-// These defines are only for this example; they are not needed in other code.
-#define TINY_GSM_USE_GPRS true
-#define TINY_GSM_USE_WIFI false
-
-
-// set GSM PIN, if any
-#define GSM_PIN ""
-
-// Your GPRS credentials, if any
-const char apn[]      = "airtelgprs.com";
-const char gprsUser[] = "";
-const char gprsPass[] = "";
-
-// Your WiFi connection credentials, if applicable
-const char wifiSSID[] = "YourSSID";
-const char wifiPass[] = "YourWiFiPass";
-
-
-#include <TinyGsmClient.h>
-#include <Ticker.h>
-#include <SPI.h>
-#include <SD.h>
-
-// Just in case someone defined the wrong thing..
-#if TINY_GSM_USE_GPRS && not defined TINY_GSM_MODEM_HAS_GPRS
-#undef TINY_GSM_USE_GPRS
-#undef TINY_GSM_USE_WIFI
-#define TINY_GSM_USE_GPRS false
-#define TINY_GSM_USE_WIFI true
-#endif
-#if TINY_GSM_USE_WIFI && not defined TINY_GSM_MODEM_HAS_WIFI
-#undef TINY_GSM_USE_GPRS
-#undef TINY_GSM_USE_WIFI
-#define TINY_GSM_USE_GPRS true
-#define TINY_GSM_USE_WIFI false
-#endif
-
-#ifdef DUMP_AT_COMMANDS
-#include <StreamDebugger.h>
-StreamDebugger debugger(SerialAT, SerialMon);
-TinyGsm        modem(debugger);
-#else
-TinyGsm        modem(SerialAT);
-#endif
-TinyGsmClient client(modem);
-PubSubClient  mqtt(client);
-
-
-
-Ticker tick;
-
-
-
-#define uS_TO_S_FACTOR 1000000ULL  // Conversion factor for micro seconds to seconds
-#define TIME_TO_SLEEP  60          // Time ESP32 will go to sleep (in seconds)
-
-#define UART_BAUD   9600
 #define PIN_DTR     25
 #define PIN_TX      27
 #define PIN_RX      26
 #define PWR_PIN     4
 
-#define SD_MISO     2
-#define SD_MOSI     15
-#define SD_SCLK     14
-#define SD_CS       13
-#define LED_PIN     12
+bool reply = false;
 
-int ledStatus = LOW;
+void modem_on() {
+  // Set-up modem  power pin
+  Serial.println("\nStarting Up Modem...");
+  pinMode(PWR_PIN, OUTPUT);
+  digitalWrite(PWR_PIN, HIGH);
+  delay(300);
+  digitalWrite(PWR_PIN, LOW);
+  delay(10000);                 //Wait for the SIM7000 communication to be normal, and will quit when receiving OK
 
-uint32_t lastReconnectAttempt = 0;
-
-
-
-void setup()
-{
-    // Set console baud rate
-    Serial.begin(115200);
-    delay(10);
-
-    // Set LED OFF
-    pinMode(LED_PIN, OUTPUT);
-    digitalWrite(LED_PIN, HIGH);
-
-    pinMode(PWR_PIN, OUTPUT);
-    digitalWrite(PWR_PIN, HIGH);
-    delay(300);
-    digitalWrite(PWR_PIN, LOW);
-
-    SPI.begin(SD_SCLK, SD_MISO, SD_MOSI, SD_CS);
-    if (!SD.begin(SD_CS)) {
-        Serial.println("SDCard MOUNT FAIL");
-    } else {
-        uint32_t cardSize = SD.cardSize() / (1024 * 1024);
-        String str = "SDCard Size: " + String(cardSize) + "MB";
-        Serial.println(str);
+  int i = 10;
+  Serial.println("\nTesting Modem Response...\n");
+  Serial.println("****");
+  while (i) {
+    SerialAT.println("AT");
+    delay(500);
+    if (SerialAT.available()) {
+      String r = SerialAT.readString();
+      Serial.println(r);
+      if ( r.indexOf("OK") >= 0 ) {
+        reply = true;
+        break;;
+      }
     }
-
-    Serial.println("\nWait...");
-
-    delay(1000);
-
-    SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
-
-    // Restart takes quite some time
-    // To skip it, call init() instead of restart()
-    Serial.println("Initializing modem...");
-    if (!modem.restart()) {
-        Serial.println("Failed to restart modem, attempting to continue without restarting");
-    }
-
-
-
-    String name = modem.getModemName();
-    DBG("Modem Name:", name);
-
-    String modemInfo = modem.getModemInfo();
-    DBG("Modem Info:", modemInfo);
-
-#if TINY_GSM_USE_GPRS
-    // Unlock your SIM card with a PIN if needed
-    if (GSM_PIN && modem.getSimStatus() != 3) {
-        modem.simUnlock(GSM_PIN);
-    }
-#endif
-
-#if TINY_GSM_USE_WIFI
-    // Wifi connection parameters must be set before waiting for the network
-    SerialMon.print(F("Setting SSID/password..."));
-    if (!modem.networkConnect(wifiSSID, wifiPass)) {
-        SerialMon.println(" fail");
-        delay(10000);
-        return;
-    }
-    SerialMon.println(" success");
-#endif
-
-#if TINY_GSM_USE_GPRS && defined TINY_GSM_MODEM_XBEE
-    // The XBee must run the gprsConnect function BEFORE waiting for network!
-    modem.gprsConnect(apn, gprsUser, gprsPass);
-#endif
-
-    SerialMon.print("Waiting for network...");
-    if (!modem.waitForNetwork()) {
-        SerialMon.println(" fail");
-        delay(10000);
-        return;
-    }
-    SerialMon.println(" success");
-
-    if (modem.isNetworkConnected()) {
-        SerialMon.println("Network connected");
-    }
-
-#if TINY_GSM_USE_GPRS
-    // GPRS connection parameters are usually set after network registration
-    SerialMon.print(F("Connecting to "));
-    SerialMon.print(apn);
-    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-        SerialMon.println(" fail");
-        delay(10000);
-        return;
-    }
-    SerialMon.println(" success");
-
-    if (modem.isGprsConnected()) {
-        SerialMon.println("GPRS connected");
-    }
-
+    delay(500);
+    i--;
+  }
+  Serial.println("****\n");
 }
 
-void loop()
-{
+void setup() {
+  Serial.begin(115200); // Set console baud rate
+  SerialAT.begin(115200, SERIAL_8N1, PIN_RX, PIN_TX);
+  delay(100);
 
+  modem_on();
+  if (reply) {
+    Serial.println(F("***********************************************************"));
+    Serial.println(F(" You can now send AT commands"));
+    Serial.println(F(" Enter \"AT\" (without quotes), and you should see \"OK\""));
+    Serial.println(F(" If it doesn't work, select \"Both NL & CR\" in Serial Monitor"));
+    Serial.println(F(" DISCLAIMER: Entering AT commands without knowing what they do"));
+    Serial.println(F(" can have undesired consiquinces..."));
+    Serial.println(F("***********************************************************\n"));
+  } else {
+    Serial.println(F("***********************************************************"));
+    Serial.println(F(" Failed to connect to the modem! Check the baud and try again."));
+    Serial.println(F("***********************************************************\n"));
+  }
+}
 
-    // Make sure we're still registered on the network
-    if (!modem.isNetworkConnected()) {
-        SerialMon.println("Network disconnected");
-        if (!modem.waitForNetwork(180000L, true)) {
-            SerialMon.println(" fail");
-            delay(10000);
-            return;
-        }
-        if (modem.isNetworkConnected()) {
-            SerialMon.println("Network re-connected");
-        }
-
-#if TINY_GSM_USE_GPRS
-        // and make sure GPRS/EPS is still connected
-        if (!modem.isGprsConnected()) {
-            SerialMon.println("GPRS disconnected!");
-            SerialMon.print(F("Connecting to "));
-            SerialMon.print(apn);
-            if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-                SerialMon.println(" fail");
-                delay(10000);
-                return;
-            }
-            if (modem.isGprsConnected()) {
-                SerialMon.println("GPRS reconnected");
-            }
-        }
-#endif
+void loop() {
+  while (true) {
+    if (SerialAT.available()) {
+      Serial.write(SerialAT.read());
     }
-
-    if (!mqtt.connected()) {
-        SerialMon.println("=== MQTT NOT CONNECTED ===");
-        // Reconnect every 10 seconds
-        uint32_t t = millis();
-        if (t - lastReconnectAttempt > 10000L) {
-            lastReconnectAttempt = t;
-            if (mqttConnect()) {
-                lastReconnectAttempt = 0;
-            }
-        }
-        delay(100);
-        return;
+    if (Serial.available()) {
+      SerialAT.write(Serial.read());
     }
-
+    delay(1);
+  }
+  Serial.println("Failed");
+  setup();
 }
