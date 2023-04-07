264cd2587ddaca4fbc2e0a3c1c2eca0c90abcd93
Saurav Kumar <67797045+rjsaurav13@users.noreply.github.com>
2023-04-07 15:11:22 +0530

    Update locationmsg.ino

--- locationmsg.ino
+++ locationmsg.ino
@@ -126,10 +126,6 @@ void loop()
 
     }
 
-    
-   
-  
-
   modem.sendSMS(SMS_TARGET, String("https://maps.google.com/?q=") + String(lat, 6) + "," + String(lon, 6));
   DBG("SMS:", res ? "OK" : "fail");
 
